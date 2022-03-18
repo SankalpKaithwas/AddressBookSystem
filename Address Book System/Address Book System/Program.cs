87b8f281e3a324f598cacc826368c60b204b06f3
unknown <kaithwas.697@gmail.com>
2022-03-18 20:23:54 +0530

    Added feature to Edit Existing Contacts

--- Address Book System/Address Book System/Program.cs
+++ Address Book System/Address Book System/Program.cs
@@ -70,7 +70,7 @@ namespace Address_Book_System
                 contactList.Add(addresses);
                 try
                 {
-                    contacts.Add(firstName, addresses);
+                    contacts.Add(firstName.ToLower(), addresses);
 
                 }
                 catch (Exception)
@@ -86,6 +86,36 @@ namespace Address_Book_System
                 }
             }
 
+            public void EditContacts(string key)
+            {
+                Console.WriteLine("Key is " + key);
+                if (contacts.ContainsKey(key))
+                {
+                    Console.WriteLine("Enter your First Name: ");
+                    string firstName = Console.ReadLine();
+                    Console.WriteLine("Enter your Last Name: ");
+                    string lastName = Console.ReadLine();
+                    Console.WriteLine("Enter your Address: ");
+                    string address = Console.ReadLine();
+                    Console.WriteLine("Enter your city: ");
+                    string city = Console.ReadLine();
+                    Console.WriteLine("Enter your State: ");
+                    string state = Console.ReadLine();
+                    Console.WriteLine("Enter your Zip: ");
+                    string zipCode = Console.ReadLine();
+                    Console.WriteLine("Enter your Phone Number: ");
+                    string phoneNumber = Console.ReadLine();
+                    Console.WriteLine("Enter your Email: ");
+                    string email = Console.ReadLine();
+                    AddressBook addresses = new AddressBook(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
+                    contactList.Add(addresses);
+                    contacts[key] = addresses;
+                }
+                else
+                {
+                    Console.WriteLine("Key doesnt exist");
+                }
+            }
 
             static void Main(string[] args)
             {
@@ -94,8 +124,9 @@ namespace Address_Book_System
                 int option = 0;
                 do
                 {
-                    Console.WriteLine("Choose 1: To add a Contact");
+                    Console.WriteLine("Choose 1: To Add a Contact");
                     Console.WriteLine("Choose 2: To get Contacts");
+                    Console.WriteLine("Choose 3: To Edit a contact");
                     Console.WriteLine("Choose 0: To Exit");
                     try
                     {
@@ -108,6 +139,11 @@ namespace Address_Book_System
                             case 2:
                                 program.GetContact();
                                 continue;
+                            case 3:
+                                Console.WriteLine("Enter key");
+                                string key = Console.ReadLine();
+                                program.EditContacts(key);
+                                break;
                             case 0:
                                 Console.WriteLine("Good Day");
                                 break;
@@ -118,8 +154,7 @@ namespace Address_Book_System
                     }
                     catch (Exception)
                     {
-
-                        Console.WriteLine("Please chhose an option");
+                        Console.WriteLine("Please choose an option");
                     }
                 } while (option != 0);
             }
